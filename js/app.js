3c3ae6f89e684f7f018c23141c5805f86fa5fb8c
Ghofran Dayyat <YOUR EMAIL ADDRESS>
2021-03-23 23:17:38 +0200

    not finish

--- js/app.js
+++ js/app.js
@@ -1,69 +1,102 @@
 'use strict';
-let allGalleryImg=[];
-let arrKeyWord=[];
+let keyArr1 = [];
+let arr1=[];
+let keyArr2 = [];
+let arr2=[];
+let totalArr= [];
+
 function Image(gallery) {
   this.image_url = gallery.image_url;
   this.title = gallery.title;
   this.description = gallery.description;
   this.keyword = gallery.keyword;
   this.horns = gallery.horns;
-  allGalleryImg.push(this);
-
 }
 
-Image.prototype.renderAuto=function() {
-  let templetClone=$('#img-temp').clone();
-  templetClone.find('h2').text(this.title);
-  templetClone.find('p').text(this.description);
-  templetClone.find('img').attr('src',this.image_url);
-  templetClone.attr('class',this.keyword);
-  templetClone.removeAttr('id','img-temp').attr('src',this.title);
-  $('main').append(templetClone);
+
+Image.prototype.creatTemplate = function (){
+  $('main').append(`
+  <div> id="unit">
+  <h2>${this.title}</h2>
+  <img src="${this.image_url}" alt="${this.title}">
+  <p>${this.dscription} </p>
+</div>`);
 };
 
-Image.prototype.filtiration = function() {
-  if(!arrKeyWord.includes(this.keyword)){
-    arrKeyWord.push(this.keyword);
-    const optionClone = $('#opt').clone();
-    optionClone.removeAttr('id', 'opt');
-    optionClone.removeAttr('value', 'default').attr('value', `${this.keyword}`);
-    optionClone.text(this.keyword);
-    $('#opt').after(optionClone);
-  }
+//----------mustach----------------//
+Image.prototype.renderTemplet =function(){
+  let templet = $('#img-temp').html();
+  let html = Mustache.render(templet, this);
+  $('main').append(html);
 };
-$('document').ready(function(){
-  // console.log('JQuery is ready');
-});
-
 const ajaxSetting = {
   method: 'get',
   dataType: 'json',
 };
 
-
 $.ajax('data/page-1.json',ajaxSetting).then((data)=>{
   data.forEach(element =>{
     let newhorn= new Image(element);
     // console.log(newhorn);
-    newhorn.renderAuto();
-    newhorn.filtiration();
+    newhorn.renderTemplet();
   });
 
+  keyArr1.forEach((element)=>{
+    $('select').append(`<option value=${element}>${element} </option>`);
+  });
 });
 
+$('select').change(function(){
+  $('main').html('');
+  let selectedOption = $(this).val();
+  totalArr = arr1+arr2;
+  totalArr.forEach(element=>{
+    if (selectedOption === element.keyword){
+      element.renderTemplet();
 
-$('#filteration').change(function() {
-  console.log( this.value);
-  allGalleryImg.forEach((element)=>{
-    console.log(element.keyword);
-    if (this.value === element.keyword){
-      $(`.${element.keyword}`).show();
-    }else if (element.keyword !== this.value){
-      $(`.${element.keyword}`).hide();
-    } else {
-      $(`.${element.keyword}`).show();
     }
   });
 });
 
+$('#page1').on('click', function(){
+  $.ajax('data/page-1.json',ajaxSetting).then((data)=>{
+    $('main').html('');
+    $('select').html('');
+    $('select').append(`
+    <option value="default">Filter by Keyword</option>`
+    );
+    data.forEach(element =>{
+      let newImg = new Image(element);
+      arr1.push(newImg);
+      if (!keyArr1.includes(newImg.keyword)){
+        keyArr1.push(newImg.keyword);
+      }
+      newImg.renderTemplet();
+    });
+    keyArr1.forEach(element=>{
+      $('select'.append(`<option value=${element}>${element}</option>`));
+    });
+  });
+});
 
+
+$('#page2').on('click', function(){
+  $.ajax('data/page-2.json',ajaxSetting).then((data)=>{
+    $('main').html('');
+    $('select').html('');
+    $('select').append(`
+    <option value="default">Filter by Keyword</option>`
+    );
+    data.forEach(element =>{
+      let newImg = new Image(element);
+      arr2.push(newImg);
+      if (!keyArr2.includes(newImg.keyword)){
+        keyArr2.push(newImg.keyword);
+      }
+      newImg.renderTemplet();
+    });
+    keyArr2.forEach(element=>{
+      $('select'.append(`<option value=${element}>${element}</option>`));
+    });
+  });
+});
